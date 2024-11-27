e0e7b094cea4ed739f1e9d0d1292530a9121416c
xXToxicCatYT <eddyguy555@gmail.com>
2024-11-27 01:37:27 -0500

    Created UPD 1 Build PC

--- Assets/Scripts/MapTextureToggler.cs
+++ Assets/Scripts/MapTextureToggler.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class MapTextureToggler : MonoBehaviour
 {
     private Material[] allMaterials; // Array to store all materials in the scene
-    private Texture[][] originalTextures; // Array to store all original textures for each material
+    private Dictionary<Material, Dictionary<string, Texture>> originalTextures = new Dictionary<Material, Dictionary<string, Texture>>();
     private bool texturesEnabled = true;
 
     void Start()
@@ -17,24 +16,21 @@ public class MapTextureToggler : MonoBehaviour
         allMaterials = GetAllMaterials(renderers);
 
         // Store original textures for all materials
-        originalTextures = new Texture[allMaterials.Length][];
-
-        for (int i = 0; i < allMaterials.Length; i++)
+        foreach (Material mat in allMaterials)
         {
-            Material mat = allMaterials[i];
             if (mat != null)
             {
-                int textureCount = ShaderUtil.GetPropertyCount(mat.shader);
-                originalTextures[i] = new Texture[textureCount];
+                var textureDictionary = new Dictionary<string, Texture>();
 
-                for (int j = 0; j < textureCount; j++)
+                // Find all texture properties in the material
+                int propertyCount = ShaderUtilRuntime.GetTexturePropertyCount(mat.shader);
+                for (int i = 0; i < propertyCount; i++)
                 {
-                    if (ShaderUtil.GetPropertyType(mat.shader, j) == ShaderUtil.ShaderPropertyType.TexEnv)
-                    {
-                        string propertyName = ShaderUtil.GetPropertyName(mat.shader, j);
-                        originalTextures[i][j] = mat.GetTexture(propertyName);
-                    }
+                    string propertyName = ShaderUtilRuntime.GetTexturePropertyName(mat.shader, i);
+                    textureDictionary[propertyName] = mat.GetTexture(propertyName);
                 }
+
+                originalTextures[mat] = textureDictionary;
             }
         }
     }
@@ -46,29 +42,23 @@ public class MapTextureToggler : MonoBehaviour
         {
             texturesEnabled = !texturesEnabled;
 
-            for (int i = 0; i < allMaterials.Length; i++)
+            foreach (Material mat in allMaterials)
             {
-                Material mat = allMaterials[i];
-                if (mat != null)
+                if (mat != null && originalTextures.ContainsKey(mat))
                 {
-                    int textureCount = ShaderUtil.GetPropertyCount(mat.shader);
-
-                    for (int j = 0; j < textureCount; j++)
+                    foreach (var texturePair in originalTextures[mat])
                     {
-                        if (ShaderUtil.GetPropertyType(mat.shader, j) == ShaderUtil.ShaderPropertyType.TexEnv)
-                        {
-                            string propertyName = ShaderUtil.GetPropertyName(mat.shader, j);
+                        string propertyName = texturePair.Key;
 
-                            if (texturesEnabled)
-                            {
-                                // Restore original texture
-                                mat.SetTexture(propertyName, originalTextures[i][j]);
-                            }
-                            else
-                            {
-                                // Disable texture
-                                mat.SetTexture(propertyName, null);
-                            }
+                        if (texturesEnabled)
+                        {
+                            // Restore original texture
+                            mat.SetTexture(propertyName, texturePair.Value);
+                        }
+                        else
+                        {
+                            // Disable texture
+                            mat.SetTexture(propertyName, null);
                         }
                     }
                 }
@@ -94,5 +84,45 @@ public class MapTextureToggler : MonoBehaviour
         }
 
         return new List<Material>(materialSet).ToArray();
+    }
+}
+
+public static class ShaderUtilRuntime
+{
+    public static int GetTexturePropertyCount(Shader shader)
+    {
+        int count = 0;
+
+        // Loop through all shader properties and count textures
+        for (int i = 0; i < shader.GetPropertyCount(); i++)
+        {
+            if (shader.GetPropertyType(i) == UnityEngine.Rendering.ShaderPropertyType.Texture)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static string GetTexturePropertyName(Shader shader, int index)
+    {
+        int texIndex = 0;
+
+        // Loop through all shader properties and find the correct texture name
+        for (int i = 0; i < shader.GetPropertyCount(); i++)
+        {
+            if (shader.GetPropertyType(i) == UnityEngine.Rendering.ShaderPropertyType.Texture)
+            {
+                if (texIndex == index)
+                {
+                    return shader.GetPropertyName(i);
+                }
+
+                texIndex++;
+            }
+        }
+
+        return null;
     }
 }
